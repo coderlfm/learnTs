a6eb7f206e39f5180b05466513a895a6d84f2b5c
宿愿Cc <375642570@qq.com>
2021-07-19 16:03:35 +0800

    feat(type): 新增 class 的继承及 public、private、protected 修饰符

---
+++ 03_class/01_class的继承.ts
@@ -0,0 +1,49 @@
+/* 
+TypeScript 中也有类的概念，
+TODO: 基本类的实现
+*/
+
+class Person {
+  name: string;
+  age: number; // 需要初始化值，否则 ts 检查报错
+
+  constructor(name: string, age: number) {
+    this.name = name; // 对于这种情况 Dart 中可以简写
+    this.age = age;
+  }
+
+  sayName() {
+    console.log(this.name);
+  }
+}
+
+const xiaomin = new Person('小明', 18);
+xiaomin.name = '小东';
+// xiaomin.age = 18;
+
+xiaomin.sayName();
+
+/**
+TODO: 类的继承
+一个类可以继承另外一个类，
+继承的类叫做 子类/派生类
+被继承的类叫做 父类/基类
+ */
+
+// Man 类中 没有 构造函数，原因是如果写了构造函数，就需要手动调用 super();
+class Man extends Person {
+  // constructor(name: string, age: number){
+  //   super(name, age)  // 如果写了构造函数，就需要手动调用 super();
+  // }
+
+  printSex() {
+    console.log('男人');
+  }
+}
+
+const man = new Man('小明', 18); //当子类没有构造函数的时，会自动调用父类的构造函数
+man.printSex();
+
+
+
+export {};
---
+++ 03_class/02_public、private、protected修饰符.ts
@@ -0,0 +1,84 @@
+// public, private, protected
+/* 
+- public (公共的)
+  在缺省的情况下，默认类中的属性和方法都是 public 修饰的
+- private (私有的)
+  私有的属性或者方法，使用 private 修饰的属性和方法只能在这个类自身内使用，不能被子类或者 new出来的示例修改
+- protected (受保护的)
+  可以在子类中访问，但是不能再实例中访问
+*/
+
+// TODO: public 和 private 修饰符的使用
+{
+  class Person {
+    // TypeScript 在类中声明的 private 只是编译阶段的 private，如果强制忽略 ts 的语法检测，
+    // 在 js 运行阶段时依旧可以获取到 prefix 属性，这是因为 JavaScript 并不支持真正意义的 private；
+    public name: string;
+    private nation = '中国';
+
+    constructor(name: string) {
+      this.name = name;
+    }
+  }
+
+  const person = new Person('小明');
+  // person.nation = '英国';  // ts 语法报错，运行阶段实际上是可以获取的
+
+  console.log(person);
+}
+
+// TODO: 子类中如何获取 父类中的 私有属性
+{
+  class Person {
+    public name: string;
+    private nation = '中国';
+
+    constructor(name: string) {
+      this.name = name;
+    }
+
+    getNation() {
+      return this.nation;
+    }
+
+    setNation(nation: string) {
+      this.nation = nation;
+    }
+  }
+
+  class Man extends Person {
+    printNation() {
+      console.log(this.getNation());
+    }
+  }
+
+  const man = new Man('小明');
+  // person.nation = '英国';  // ts 语法报错，运行阶段实际上是可以获取的
+  // man.printNation();
+
+  // 通过调用子类中的方法来获取，或者实例也可以直接调用 getNation() 因为 该函数的声明是缺省的，默认是 public，如果将它的修饰符改为 protected 则实例无法调用
+  console.log(man.getNation());
+}
+
+// TODO: protected 修饰符的使用
+{
+  class Person {
+    public name: string;
+    private nation = '中国';
+    protected sex: string; // protected 修饰的属性可以在子类中访问，但是实例上不可以访问
+
+    constructor(name: string, sex: string) {
+      this.name = name;
+      this.sex = sex;
+    }
+  }
+
+  class Man extends Person {
+    constructor(name: string) {
+      super(name, 'man');
+    }
+  }
+
+  const man = new Man('小明');
+  // man.sex = 'woman'; // ts 语法报错，实例不能访问受保护的类型属性或方法
+}
