565e52ddc7f92a064393a87b9aa30c4e579f1b10
宿愿Cc <375642570@qq.com>
2021-07-20 10:51:20 +0800

    feat(type): 新增 interface 函数类型

---
+++ 04_interface/02_interface 函数类型.ts
@@ -0,0 +1,13 @@
+/* 
+interface 也可以用来声明函数类型，但是多数情况下我们会使用 type 类型别名来声明函数类型
+*/
+
+{
+  interface ISum {
+    (num1: number, num2: number): number;
+  }
+
+  const sum: ISum = (num1, num2) => {
+    return num1 + num2;
+  };
+}
