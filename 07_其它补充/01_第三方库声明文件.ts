09f36dfb966d56954a5db97ced6a46dd84fbd250
宿愿Cc <375642570@qq.com>
2021-07-23 17:38:25 +0800

    feat(type): 新增 @types 第三方库类型声明文件

---
+++ 07_其它补充/01_第三方库声明文件.ts
@@ -0,0 +1,10 @@
+/* 
+  当我们在实际项目开发的时候，我们会从 `npm` 上使用很多第三方库，有很多历史悠久的库是没有声明文件的，因为写类型声明文件非常耗费精力，[DefinitelyTyped](https://github.com/DefinitelyTyped) 是有社区维护的一个 `TypeScript` 类型文件类库，[DefinitelyTyped](https://github.com/DefinitelyTyped) 基本上以及覆盖了社区 90% 的 JavaScript 库的类型声明
+  [DefinitelyTyped](https://github.com/DefinitelyTyped) 维护的库都在 `@types` 下，
+
+  在 [这里](https://www.typescriptlang.org/dt/search?search=) 可以搜索到我们要使用的库是否有声明文件
+
+  使用 
+  npm install @types/React --save-dev
+  例如：`jQuery`，`React` 等，都是社区在维护的 
+*/
